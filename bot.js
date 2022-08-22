57a926ea4d5888b526af3cb914ec0081c0fc9563
Pasta <umar.ansari7331@gmail.com>
2022-08-22 21:41:14 +0530

    working model

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 
 var stop = 0;
 
-bot.command('trade', async(ctx) => {
+bot.command('trade', async (ctx) => {
 
   let market_data = await CoinGeckoClient.global();
   let market_cap_change_percentage_24h = market_data.data.data.market_cap_change_percentage_24h_usd;
@@ -31,32 +31,37 @@ bot.command('check', (ctx) => {
 
 })
 
-bot.command('stop', () => { stop = 1; Telegraf.reply(`Stop = ${stop}`)})    //When stop is enabled the main function will run only once
+bot.command('stop', (ctx) => { stop = 1; ctx.reply(`Stop = ${stop}`) })    //When stop is enabled the main function will run only once
 
-bot.command('resume', () => { stop = 0; Telegraf.reply(`Stop = ${stop}`) })  //When stop is disables the main function will run until stop is enabled 
+bot.command('resume', (ctx) => { stop = 0; ctx.reply(`Stop = ${stop}`) })  //When stop is disables the main function will run until stop is enabled 
 
 bot.launch();
 
 
-async function func(coins,ctx) {
+async function func(coins, ctx) {
 
   for (var i = 0; i < myCoins.length; i++) {
-   const crypto = await CoinGeckoClient.coins.fetch(myCoins[i], {});
+    const crypto = await CoinGeckoClient.coins.fetch(myCoins[i], {});
 
-    
+
     const coinName = crypto.data.id;
-    const one_hour_growth = crypto.data.market_data.price_change_percentage_1h_in_currency['inr'];
+    const one_hour_growth = crypto.data.market_data.price_change_percentage_1h_in_currency.inr;
     const coin_price = crypto.data.market_data.current_price['inr'];
 
-
+    try {
+      ctx.reply(one_hour_growth)
+    }
+    catch (err) {
+      console.log(err)
+    }
     // ctx.reply(`${coinName} \n Change Percentage in ${coinName} 1 hour: ${one_hour_growth} %`);
 
     // let a = await checkLimit('cardano', 40);
     // ctx.replyWithMarkdown(a)
-    
+
     try {
 
-        if (one_hour_growth >= 2) {
+      if (one_hour_growth >= 2) {
         ctx.reply(`ALERT!!! ${coinName} went up \nThe Price of ${coinName} is ${coin_price} `);
       }
 
@@ -64,7 +69,7 @@ async function func(coins,ctx) {
         ctx.reply(`DOWN DOWN!!! ${coinName} went DOWN \nThe Price of ${coinName} is ${coin_price} `);
       }
 
-    } catch(error) {
+    } catch (error) {
       console.log(error)
     }
 
@@ -73,24 +78,24 @@ async function func(coins,ctx) {
 
   if (stop === 1) {
     return
-  }else
-   var timeOut = setTimeout(func, 15000, coins, ctx)
+  } else
+    var timeOut = setTimeout(func, 120000, coins, ctx)
 
 
 }
 
 // Give an upper limit and the coin name to the function and it will send an alert message if the coin cross the upperlimit
 async function checkLimit(coin, limit) {
- const crypto = await CoinGeckoClient.coins.fetch(coin, {});
+  const crypto = await CoinGeckoClient.coins.fetch(coin, {});
 
- let coindata = await CoinGeckoClient.simple.price({
-      ids: [coin],
-      vs_currencies: ['inr']
+  let coindata = await CoinGeckoClient.simple.price({
+    ids: [coin],
+    vs_currencies: ['inr']
   });
 
- if (coindata.data.cardano.inr >= limit) {
-  return (`Upppppppp! \n *${coin.toUpperCase()}* is finally UPPPP!`);
- }
+  if (coindata.data.cardano.inr >= limit) {
+    return (`Upppppppp! \n *${coin.toUpperCase()}* is finally UPPPP!`);
+  }
 }
 
 // removed graceful stop
--- index.js
+++ index.js
@@ -5,79 +5,83 @@ const CoinGecko = require('coingecko-api');
 //2. Initiate the CoinGecko API Client
 const CoinGeckoClient = new CoinGecko();
 
+const a = async() => {
+  const crypto = await CoinGeckoClient.coins.fetch('flow', {});
+
+    const one_hour_growth = crypto.data.market_data.  ;
+    console.log(one_hour_growth)
+}
+   
+
+a()
+
+// var myCoins = ['solana', 'apecoin', 'polkadot', 'polygon','flow'];
+
+
+// //3. Make calls
+// var func = async(coins) => {
+
+//   // DONE:: THIS IS FOR THE WHOLE MARKET
+//   // let market_cap_change_percentage_24h = await CoinGeckoClient.global();
+//   // console.log(market_cap_change_percentage_24h.data.data.market_cap_change_percentage_24h_usd)
+
+//    // const crypto = await CoinGeckoClient.coins.fetch('cardano', {});
+//    // const lol = crypto.data.market_data.price_change_percentage_1h_in_currency['inr'];
+//    // console.log(lol);
+
+
+//   // for (var i = 0; i < myCoins.length; i++) {
+
+//   //   const crypto = await CoinGeckoClient.coins.fetch(coins[i], {});
+
+//   //   const coinName = crypto.data.id;
+//   //   const one_hour_growth = crypto.data.market_data.price_change_percentage_1h_in_currency['inr'];
+//   //   const coin_price = crypto.data.market_data.current_price['inr'];
+
+//   //   console.log(coinName);
+//   //   console.log(`Change Percentage in 1 hour: ${one_hour_growth} \n ${coin_price} `);
+
+//   //   if (one_hour_growth >= 0.1) {
+//   //     console.log(`ALERT!!! ${coinName} went up  `);
+//   //     console.log(`The Price of ${coinName} is ${coin_price} `)
+//   //     clearInterval(interval);
+//   //   }
+
+//   //   console.log("\n")
+//   // }
 
 
 
+//   // DONE:: FOR A SPECIFIC COIN, TODO: PUT THIS IN A FUCNTION AND PASS THE NAME OF THE COIN AS PARAMETER TO GET THE VALUE OF THE REQUSTED COIN
+//   // let cardano = await CoinGeckoClient.coins.fetch('cardano', {});
+//   // console.log(cardano.data.tickers[1]);                      //cardano.data.market_data.current_price['inr'], cardano.data.id, cardano.data.market_cap_rank, cardano.data.liquidity_score, cardano.data.market_data.high_24h['inr'], cardano.data.market_data.low_24h['inr'], cardano.data.market_data.price_change_percentage_1h_in_currency['inr'], price_change_percentage_24h_in_currency[inr]
+//   // console.log(cardano.data.market_data.price_change_percentage_1h_in_currency['inr']);
 
-var myCoins = ['solana', 'apecoin', 'polkadot', 'polygon','flow'];
+//   // JUST SHOWS THE PRICE NOTHING MORE
+//   // let data = await CoinGeckoClient.simple.price({
+//   //     ids: ['avalanche', 'near', 'apecoin', 'polkadot', 'matic-network','shiba-inu','ethereum-classic', 'flow', 'ethereum-classic'],
+//   //     vs_currencies: ['inr']
+//   // });
 
+//   let data = await CoinGeckoClient.simple.price({
+//       ids: ['cardano'],
+//       vs_currencies: ['inr']
+//   });
 
-//3. Make calls
-var func = async(coins) => {
-
-  // DONE:: THIS IS FOR THE WHOLE MARKET
-  // let market_cap_change_percentage_24h = await CoinGeckoClient.global();
-  // console.log(market_cap_change_percentage_24h.data.data.market_cap_change_percentage_24h_usd)
-
-   // const crypto = await CoinGeckoClient.coins.fetch('cardano', {});
-   // const lol = crypto.data.market_data.price_change_percentage_1h_in_currency['inr'];
-   // console.log(lol);
-
-
-  // for (var i = 0; i < myCoins.length; i++) {
-
-  //   const crypto = await CoinGeckoClient.coins.fetch(coins[i], {});
-
-  //   const coinName = crypto.data.id;
-  //   const one_hour_growth = crypto.data.market_data.price_change_percentage_1h_in_currency['inr'];
-  //   const coin_price = crypto.data.market_data.current_price['inr'];
-
-  //   console.log(coinName);
-  //   console.log(`Change Percentage in 1 hour: ${one_hour_growth} \n ${coin_price} `);
-
-  //   if (one_hour_growth >= 0.1) {
-  //     console.log(`ALERT!!! ${coinName} went up  `);
-  //     console.log(`The Price of ${coinName} is ${coin_price} `)
-  //     clearInterval(interval);
-  //   }
-
-  //   console.log("\n")
-  // }
-
-
-
-  // DONE:: FOR A SPECIFIC COIN, TODO: PUT THIS IN A FUCNTION AND PASS THE NAME OF THE COIN AS PARAMETER TO GET THE VALUE OF THE REQUSTED COIN
-  // let cardano = await CoinGeckoClient.coins.fetch('cardano', {});
-  // console.log(cardano.data.tickers[1]);                      //cardano.data.market_data.current_price['inr'], cardano.data.id, cardano.data.market_cap_rank, cardano.data.liquidity_score, cardano.data.market_data.high_24h['inr'], cardano.data.market_data.low_24h['inr'], cardano.data.market_data.price_change_percentage_1h_in_currency['inr'], price_change_percentage_24h_in_currency[inr]
-  // console.log(cardano.data.market_data.price_change_percentage_1h_in_currency['inr']);
-
-  // JUST SHOWS THE PRICE NOTHING MORE
-  // let data = await CoinGeckoClient.simple.price({
-  //     ids: ['avalanche', 'near', 'apecoin', 'polkadot', 'matic-network','shiba-inu','ethereum-classic', 'flow', 'ethereum-classic'],
-  //     vs_currencies: ['inr']
-  // });
-
-  let data = await CoinGeckoClient.simple.price({
-      ids: ['cardano'],
-      vs_currencies: ['inr']
-  });
-
-  console.log(data.data.cardano.inr)
+//   console.log(data.data.cardano.inr)
  
-  // let coi = await CoinGeckoClient.coins.all();
-  // console.log(coi)
-};
+//   // let coi = await CoinGeckoClient.coins.all();
+//   // console.log(coi)
+// };
 
 
-// func(myCoins);
+// // func(myCoins);
 
-// var interval = setInterval(func, 9000, myCoins);
-func(myCoins)
+// // var interval = setInterval(func, 9000, myCoins);
+// func(myCoins)
 
-// 1.] coins.markets()   [  let coins = await CoinGeckoClient.coins.markets() ]
-// Use this to obtain all the coins market data (price, market cap, volume).
-
-
+// // 1.] coins.markets()   [  let coins = await CoinGeckoClient.coins.markets() ]
+// // Use this to obtain all the coins market data (price, market cap, volume).
 
 
 
